bb076b4138eda310110bec5cbd746d31067e6538
AhmadJaber <jaber131706@gmail.com>
2021-01-08 15:00:42 +0600

    :tada: initial setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e904177fa4a7fa74d3d4a097559165a0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24dd127f-0942-4c41-9df9-a9a0364dbf22'
+};
